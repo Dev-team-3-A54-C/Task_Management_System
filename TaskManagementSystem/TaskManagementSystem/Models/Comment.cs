13cbd573dd978f02731e9602073ffa1081b4a936
Hristian-N <hristian.kr.nikolov@gmail.com>
2023-10-10 14:50:35 +0300

    Implemented IFeedback interface and created EventLog and Member classes

--- TaskManagementSystem/TaskManagementSystem/Models/Comment.cs
+++ TaskManagementSystem/TaskManagementSystem/Models/Comment.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagementSystem.Models.Contracts;
 
 namespace TaskManagementSystem.Models
 {
-    public class Comment
+    public class Comment : IComment
     {
+        public string Message => throw new NotImplementedException();
+
+        public string Author => throw new NotImplementedException();
     }
 }
---
+++ TaskManagementSystem/TaskManagementSystem/Models/Contracts/IFeedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Models.Contracts
+{
+    public interface IFeedback : ITask
+    {
+        int Rating { get;}
+        FeedbackStatus status { get;}
+    }
+}
