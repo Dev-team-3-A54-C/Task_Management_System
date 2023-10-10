66396bfb3513597a731df4ba22a31e7b5ea73f61
Hristian-N <hristian.kr.nikolov@gmail.com>
2023-10-10 13:43:38 +0300

    Implemented ITask interface

---
+++ TaskManagementSystem/TaskManagementSystem/Models/Contracts/ITask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Models.Contracts
+{
+    public interface ITask
+    {
+        int Id { get; }
+
+        string Title { get;}
+
+        string Description { get;}
+    }
+}
