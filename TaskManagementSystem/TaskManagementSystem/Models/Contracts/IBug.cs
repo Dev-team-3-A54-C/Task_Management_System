5ec336be7730a3ff210f912e2c35856cc98b014b
Hristian-N <hristian.kr.nikolov@gmail.com>
2023-10-10 13:42:07 +0300

    Implemented IBug interface

---
+++ TaskManagementSystem/TaskManagementSystem/Models/Contracts/IBug.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Models.Contracts
+{
+    internal interface IBug : ITask, IHasPriority
+    {
+        IList<string> Steps { get; }
+        Severity Severity { get; }
+        BugStatus Status { get; }
+        Member Assignee { get; }
+        IList<string> Comments { get; }
+        IList<string> History { get; }
+    }
+}
