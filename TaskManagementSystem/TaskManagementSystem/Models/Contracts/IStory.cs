0ca8b5914c95a03e99e14486f2d605bbe6c38389
Hristian-N <hristian.kr.nikolov@gmail.com>
2023-10-10 14:17:52 +0300

    Implemented IStory interface

---
+++ TaskManagementSystem/TaskManagementSystem/Models/Contracts/IStory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Models.Contracts
+{
+    public interface IStory : ITask, IHasPriority, HasAssignee
+    {
+        StorySize Size { get; }
+        StoryStatus Status { get; }
+    }
+}
