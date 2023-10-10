a26ab55ee22a414a672fb52e5d11d030c30333de
Hristian-N <hristian.kr.nikolov@gmail.com>
2023-10-10 13:49:00 +0300

    Renamed "ValidationHelpers" to "ValidationHelper" because of naming conflict with the folder

---
+++ TaskManagementSystem/TaskManagementSystem/ValidationHelpers/ValidationHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementSystem.Exceptions;
+
+namespace TaskManagementSystem.ValidationHelpers
+{
+    public static class ValidationHelper
+    {
+        public static void ValidateStringLength(string value, int minValue, int maxValue, string message)
+        {
+            if (value.Length < minValue || value.Length > maxValue)
+                throw new InvalidUserInput(message);
+        }
+    }
+}
