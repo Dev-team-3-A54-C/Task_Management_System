1db8e5530652484ab1764518dc9d80ff18fb4868
Hristian-N <hristian.kr.nikolov@gmail.com>
2023-10-10 13:40:17 +0300

    Implemented Priority enum

---
+++ TaskManagementSystem/TaskManagementSystem/Models/Priority.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Models
+{
+    internal enum Priority
+    {
+        High,
+        Medium,
+        Low
+    }
+}
