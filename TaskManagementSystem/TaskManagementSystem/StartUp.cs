5a260a6da8774f01842e304e07d555d6de67320c
Nikolay Dobrev <nickndobrev@gmail.com>
2023-10-10 10:40:48 +0300

    Created base console app

---
+++ TaskManagementSystem/TaskManagementSystem/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagementSystem
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
