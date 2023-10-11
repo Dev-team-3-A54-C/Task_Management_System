1a40d642effdc77817a3c18b0546284bb63d4f82
NikolaIv <poormuggle6@gmail.com>
2023-10-11 11:34:58 +0300

    Added needed functionality to IRepository, added new collections to Repository

--- TaskManagementSystem/TaskManagementSystem/Core/Contracts/IRepository.cs
+++ TaskManagementSystem/TaskManagementSystem/Core/Contracts/IRepository.cs
@@ -10,16 +10,32 @@ namespace TaskManagementSystem.Core.Contracts
 {
     public interface IRepository
     {
-        void AddTeam(Team t);
-
-        void RemoveTeam(Team t);
 
         bool TeamExists(string teamName);
 
-        ITeam GetTeam(string teamName);
 
+        ITeam CreateTeam(string name);
         IComment CreateComment(string content, string author);
-
         ITask CreateTask(string title, string description);
+        IMember CreatePerson(string name);
+        IBoard CreateBoard(string name); //?
+
+        ITeam GetTeam(string teamName);
+        IMember GetMember(string name);
+        IBoard GetBoard(string name);
+
+        ITask GetTask(string name);
+
+        
+        
+        void ChangeBugPriority(IBug bug);
+        void ChangeBugSeverity(IBug bug);
+        void ChangeBugStatus(IBug bug);
+        void ChangeStoryPriority(IStory story);
+        void ChangeStoryStatus(IStory story);
+
+        void ChangeStorySize(IStory story);
+        void ChangeRating(IFeedback feedback);
+        void ChangeStatus(IFeedback feedback);
     }
 }
--- TaskManagementSystem/TaskManagementSystem/Core/Repository.cs
+++ TaskManagementSystem/TaskManagementSystem/Core/Repository.cs
@@ -12,6 +12,9 @@ namespace TaskManagementSystem.Core
     public class Repository : IRepository
     {
         private readonly IList<ITeam> teams = new List<ITeam>();
+        private readonly IList<IMember> members = new List<IMember>();
+        private readonly IList<IBoard> boards = new List<IBoard>();
+        private readonly IList<ITask> tasks = new List<ITask>();
 
         public IList<ITeam> Teams
         {
@@ -20,7 +23,58 @@ namespace TaskManagementSystem.Core
                 return new List<ITeam>(teams);
             }
         }
+        public IList<IMember> Members
+        {
+            get
+            {
+                return new List<IMember>(members);
+            }
+        }
+        public IList<IBoard> Boards
+        {
+            get
+            {
+                return new List<IBoard>(boards);
+            }
+        }
+        public IList<ITask> Tasks
+        {
+            get
+            {
+                return new List<ITask>(tasks);
+            }
+        }
         public void AddTeam(Team t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeRating(IFeedback feedback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeStatus(IFeedback feedback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeStoryPriority(IStory story)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeStorySize(IStory story)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeStoryStatus(IStory story)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBoard CreateBoard(string name)
         {
             throw new NotImplementedException();
         }
@@ -30,7 +84,37 @@ namespace TaskManagementSystem.Core
             throw new NotImplementedException();
         }
 
+        public IMember CreateNewPerson(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMember CreatePerson(string name)
+        {
+            throw new NotImplementedException();
+        }
+
         public ITask CreateTask(string title, string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITeam CreateTeam(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBoard GetBoard(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMember GetMember(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITask GetTask(string name)
         {
             throw new NotImplementedException();
         }
@@ -45,7 +129,27 @@ namespace TaskManagementSystem.Core
             throw new NotImplementedException();
         }
 
+        public string ShowAllTeams()
+        {
+            throw new NotImplementedException();
+        }
+
         public bool TeamExists(string teamName)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository.ChangeBugPriority(IBug bug)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository.ChangeBugSeverity(IBug bug)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository.ChangeBugStatus(IBug bug)
         {
             throw new NotImplementedException();
         }
