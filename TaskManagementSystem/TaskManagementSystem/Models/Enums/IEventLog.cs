b2b30836b69a45c0f8658e9b5f6af47423860075
Hristian-N <hristian.kr.nikolov@gmail.com>
2023-10-11 10:17:26 +0300

    Created IEventLog, and changed Board, EventLog, Task, Team

--- TaskManagementSystem/TaskManagementSystem/Models/Enums/IEventLog.cs
+++ TaskManagementSystem/TaskManagementSystem/Models/Enums/IEventLog.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TaskManagementSystem.Models.Enums;
 
-namespace TaskManagementSystem.Models
+namespace TaskManagementSystem.Models.Enums
 {
-    public class EventLog : IEventLog
+    public interface IEventLog
     {
     }
 }
