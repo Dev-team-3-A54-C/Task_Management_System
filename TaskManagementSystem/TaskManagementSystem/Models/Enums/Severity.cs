aa3629429b8069129495c42fa2e8a1cb1b96be9f
Hristian-N <hristian.kr.nikolov@gmail.com>
2023-10-11 10:08:50 +0300

    Moved enums into a single folder again

--- TaskManagementSystem/TaskManagementSystem/Models/Enums/Severity.cs
+++ TaskManagementSystem/TaskManagementSystem/Models/Enums/Severity.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TaskManagementSystem.Models
+namespace TaskManagementSystem.Models.Enums
 {
     internal enum Severity
     {
