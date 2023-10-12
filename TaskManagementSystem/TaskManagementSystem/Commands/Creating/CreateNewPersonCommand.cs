1bb22c6593347a66c2f1cc78e0daa2ce9e2f1c88
Nikolay Dobrev <nickndobrev@gmail.com>
2023-10-12 17:13:32 +0300

    Implemented "CreateNewPersonCommand"

--- TaskManagementSystem/TaskManagementSystem/Commands/Creating/CreateNewPersonCommand.cs
+++ TaskManagementSystem/TaskManagementSystem/Commands/Creating/CreateNewPersonCommand.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagementSystem.Core.Contracts;
+using TaskManagementSystem.Exceptions;
 
 namespace TaskManagementSystem.Commands.Creating
 {
-    public class CreateNewPersonCommand
+    public class CreateNewPersonCommand : BaseCommand
     {
-        //ToDo
+        public const int ExpectedNumberOfArguments = 1;
+
+        public CreateNewPersonCommand(IList<string> commandParameters, IRepository repository) : base(commandParameters, repository)
+        {
+        }
+        public override string Execute()
+        {
+            if(this.CommandParameters.Count != ExpectedNumberOfArguments)
+            {
+                throw new InvalidUserInputException($"Invalid number of arguments. Expected: {ExpectedNumberOfArguments}, Received: {this.CommandParameters.Count}");
+            }
+
+            string personName = this.CommandParameters[0];
+
+            var person = this.Repository.CreatePerson(personName);
+
+            return $"Person with the name {personName} was created";
+        }
     }
 }
