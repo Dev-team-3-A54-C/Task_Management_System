f31988895ba672cf6f701166fcd1b41520c522fb
Nikolay Dobrev <nickndobrev@gmail.com>
2023-10-13 11:02:02 +0300

    Implemented "CreateNewFeedbackInBoardCommand" and "CreateNewStoryInBoardCommand".

--- TaskManagementSystem/TaskManagementSystem/Commands/Creating/CreateNewFeedbackInBoardCommand.cs
+++ TaskManagementSystem/TaskManagementSystem/Commands/Creating/CreateNewFeedbackInBoardCommand.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagementSystem.Core.Contracts;
+using TaskManagementSystem.Exceptions;
+using TaskManagementSystem.Models.Enums;
 
 namespace TaskManagementSystem.Commands.Creating
 {
-    public class CreateNewFeedbackInBoardCommand
+    public class CreateNewFeedbackInBoardCommand : BaseCommand
     {
-        //ToDo
+        public const int ExpectedNumberOfArguments = 4;
+
+        public CreateNewFeedbackInBoardCommand(IList<string> commandParameters, IRepository repository) : base(commandParameters,repository)
+        {
+               
+        }
+        public override string Execute()
+        {
+            if (this.CommandParameters.Count != ExpectedNumberOfArguments)
+            {
+                throw new InvalidUserInputException($"Invalid number of arguments. Expected: {ExpectedNumberOfArguments}, Received: {this.CommandParameters.Count}.");
+            }
+
+            string feedbackTitle = CommandParameters[0];
+            string feedbackDescription = CommandParameters[1];
+            int feedbackRating = base.ParseIntParameter(CommandParameters[2], "Rating");
+
+            string boardName = CommandParameters[3];
+
+            var newFeedback = this.Repository.CreateFeedback(feedbackTitle,feedbackDescription, feedbackRating);
+
+            var board = this.Repository.GetBoard(boardName);
+            if (board == null)
+            {
+                throw new InvalidBoardException($"Board with name '{boardName}' does not exist");
+            }
+
+            board.AddTask(newFeedback);
+
+            return $"New feedback with title '{feedbackTitle}' was created and added to board '{boardName}'";
+        }
     }
 }
--- TaskManagementSystem/TaskManagementSystem/Commands/Creating/CreateNewStoryInBoardCommand.cs
+++ TaskManagementSystem/TaskManagementSystem/Commands/Creating/CreateNewStoryInBoardCommand.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagementSystem.Core.Contracts;
+using TaskManagementSystem.Exceptions;
+using TaskManagementSystem.Models.Enums;
 
 namespace TaskManagementSystem.Commands.Creating
 {
-    public class CreateNewStoryInBoardCommand
+    public class CreateNewStoryInBoardCommand : BaseCommand
     {
-        //ToDo
+        public const int ExpectedNumberOfArguments = 5;
+        public CreateNewStoryInBoardCommand(IList<string> commandParameters, IRepository repository) : base(commandParameters, repository)
+        {
+            
+        }
+        public override string Execute()
+        {
+            if (this.CommandParameters.Count != ExpectedNumberOfArguments)
+            {
+                throw new InvalidUserInputException($"Invalid number of arguments. Expected: {ExpectedNumberOfArguments}, Received: {this.CommandParameters.Count}.");
+            }
+
+            string storyTitle = CommandParameters[0];
+            string storyDescription = CommandParameters[1];
+            PriorityType storyPriority = base.ParsePriorityParameter(this.CommandParameters[2], "Priority");
+            SizeType storySize = base.ParseSizeParameter(this.CommandParameters[3], "Size");
+
+            string boardName = CommandParameters[4];
+
+            var newStory = this.Repository.CreateStory(storyTitle,storyDescription,storySize,storyPriority);
+
+            var board = this.Repository.GetBoard(boardName);
+            if (board == null)
+            {
+                throw new InvalidBoardException($"Board with name '{boardName}' does not exist");
+            }
+
+            board.AddTask(newStory);
+
+            return $"New story with title '{storyTitle}' was created and added to board '{boardName}'";
+        }
     }
 }
