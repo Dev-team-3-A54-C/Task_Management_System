0fa28b47f3e2d808c98870881d653495e09cbf23
Hristian-N <hristian.kr.nikolov@gmail.com>
2023-10-16 14:07:31 +0300

    Implemented DuplicateItemException for checking if an item already exists inside team's board and member lists

--- TaskManagementSystem/TaskManagementSystem/Models/Enums/TaskType.cs
+++ TaskManagementSystem/TaskManagementSystem/Models/Enums/TaskType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TaskManagementSystem.Models.Contracts
+namespace TaskManagementSystem.Models.Enums
 {
     public enum TaskType
     {
--- TaskManagementSystem/TaskManagementSystem/Models/Team.cs
+++ TaskManagementSystem/TaskManagementSystem/Models/Team.cs
@@ -5,6 +5,7 @@ using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagementSystem.Exceptions;
 using TaskManagementSystem.Models.Contracts;
 using TaskManagementSystem.Models.Enums;
 
@@ -44,12 +45,18 @@ namespace TaskManagementSystem.Models
 
         public void AddMember(IMember member)
         {
+            if (members.Contains(member))
+                throw new DuplicateItemException($"Member with name \"{member.Name}\" already exist in the team \"{Name}\"");
+
             members.Add(member);
             AddEventToLog($"Added \"{member.Name}\" to team \"{Name}\"");
         }
 
         public void AddBoard(IBoard board)
         {
+            if (boards.Contains(board))
+                throw new DuplicateItemException($"Board with name \"{board.Name}\" already exist in the team \"{Name}\"");
+
             boards.Add(board);
             AddEventToLog($"Added \"{board.Name}\" to team \"{Name}\"");
         }
